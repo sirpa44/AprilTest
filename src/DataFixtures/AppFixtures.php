3b8bd16961dd265c1f0e81864ab194c7ee24883f
Antoine <sirpa.antoine@gmail.com>
2024-03-27 13:12:36 +0100

    create Prospect fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,15 +2,27 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Prospect;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $faker = Factory::create('fr_FR');
+
+
+        for ($i = 0; $i < 20; $i++) {
+            $prospect = new Prospect;
+            $prospect->setLastName($faker->lastName());
+            $prospect->setFirstName($faker->firstName());
+            $prospect->setPhone($faker->phoneNumber());
+            $prospect->setEmail($faker->email());
+
+            $manager->persist($prospect);
+        }
 
         $manager->flush();
     }
